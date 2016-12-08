ae11e1272e84c25ff5a0296ed473e7749873de99
Pin Lin <moneycat711@gmail.com>
2016-12-08 22:08:04 +0800

    我已經做好了（？？？

---
+++ app.js
@@ -0,0 +1,61 @@
+//可調整的設定
+var servicePort = 8004;
+
+//引入模組
+var express = require("express");
+var app = express();
+var fs = require("fs");
+
+//靜態資源目錄
+app.use("/static",express.static(__dirname));
+
+//讀取json檔
+var files = "";
+fs.readFile("./namelist.json", "utf-8", function(err, data){
+     if (err) {
+          console.error(err);
+     } else {
+        files = JSON.parse(data);
+        console.log("Read JSON File Successfully.");
+     }
+});
+
+//首頁
+app.get("/",function(req,res){
+	res.sendFile(__dirname + "/index.html");
+});
+
+//接收到get請求
+app.get("/calc/:name",function(req,res){
+    var result = files[req.params.name];
+    console.log("Look for " + req.params.name + "'s Master: " + result);
+    if (result != undefined) {
+        //正常輸出
+        res.send("<!DOCTYPE html>" +
+                 "<html>" +
+                 "  <script>" +
+                 "      function helloYou() {" +
+                 "          alert('" + result + "');" +
+                 "          location.href='/';" +
+                 "      }" +
+                 "  </script>" +
+                 "  <body onload='helloYou()'/>" +
+                 "</html>" 
+                );
+    }
+    else {
+        //輸入錯名字
+        res.sendFile(__dirname + "/error.html");
+    }
+});
+
+//Not Found
+app.use(function(req,res){
+	res.sendFile(__dirname + "/error.html");
+});
+
+//開啟伺服器
+app.listen(servicePort,function(err){
+	console.log("Server Path: " + __dirname);
+	console.log("Listening on Port " + servicePort);
+});
